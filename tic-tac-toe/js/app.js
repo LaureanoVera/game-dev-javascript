2b5b0ae5250cd14eed2da2195cccca60b350c93d
LaureanoVera <laureanoigvera@gmail.com>
2021-07-15 10:55:32 -0300

    reconocer el indice de las celdas

--- tic-tac-toe/js/app.js
+++ tic-tac-toe/js/app.js
@@ -1,6 +1,6 @@
-const statusDisplay = document.querySelector("game__notification");
+const statusDisplay = document.querySelector(".game__notification");
 const gameState = ["", "", "", "", "", "", "", "", ""];
-const conditions = [
+const CONDITIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -11,6 +11,41 @@ const conditions = [
   [6, 4, 2],
 ];
 
-const victory = () => `Player ${current} won!`
-const draw = () => `The game was a draw`
-const turn = () => `Player turn: ${current}`
+const victory = () => `Player ${current} won!`;
+const draw = () => `The game was a draw`;
+const turn = () => `Player turn: ${current}`;
+
+let gameRun = true;
+let current = "O";
+
+// FUNCTIONS
+const handleCell = evt => {
+  const cell = evt.target
+  if (cell.classList.contains('game__cell')) {
+    const cellIndex = Array.from(cell.parentNode.children).indexOf(cell)
+    console.log(cellIndex);
+    if (gameState[cellIndex] != "" || !gameRun) {
+
+    }
+  }
+  console.log(cell);
+}
+const restartGame = () => {
+  console.log('CLICK');
+}
+const listeners = () => {
+  document
+    .querySelector(".game__container")
+    .addEventListener("click", handleCell);
+  document.querySelector(".game__restart").addEventListener("click", restartGame);
+};
+
+const handleStatus = (msg) => {
+  statusDisplay.innerHTML = msg;
+};
+
+const main = () => {
+  handleStatus(turn());
+  listeners();
+};
+main()
